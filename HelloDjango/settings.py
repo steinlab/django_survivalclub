a4467647401ad270ef6c81141fb22bf2610f7132
unknown <C:\Users\steindev\AppData\Roaming\The Bat!>
2017-11-06 23:01:16 +0300

    fix

--- HelloDjango/settings.py
+++ HelloDjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l$cc2b@q4y*0ku5up#8n@y@4b@6yvbn)$-l!v1pwqcqzj&@-&p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['dev.survivalclub.ru']
 
