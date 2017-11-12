daa8ff19a3c1457de2c3ff714fb0d9c283d79ff0
steinlab <balor@bk.ru>
2017-11-13 01:38:13 +0300

    update

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from .models import Post
+from PIL import Image
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'text',)
+        fields = ('image', 'title', 'text')
