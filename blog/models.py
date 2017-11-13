85897f576dc91fbbd91a79d5810bf95dc5189901
steinlab <balor@bk.ru>
2017-11-13 16:54:51 +0300

    fix

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,13 @@
-﻿# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from decimal import Decimal
 from django.db import models
 from django.utils import timezone
-from PIL import Image
-
+from PIL import Image  
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
-    image = models.ImageField(upload_to='images',blank=True,verbose_name='Загрузить изображение поста')
+    image = models.ImageField(upload_to='images', blank=True, verbose_name='Загрузить изображение поста')
     title = models.CharField(max_length=400)
     text = models.TextField()
     created_date = models.DateTimeField(
@@ -17,7 +16,7 @@ class Post(models.Model):
             blank=True, null=True)
 
     def publish(self):
-        self.published_date = timezone.now()
+        self.published_date=timezone.now()
         self.save()
 
     def __unicode__(self):
